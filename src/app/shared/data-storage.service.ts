732cea83399e2486be793717c09c228ab1071ee6
Anna Leheta <annaleheta@LehetasMac.local>
2019-11-06 11:38:37 -0500

    Improve with added Http

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+import { map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+    constructor(private http: HttpClient,
+                private recipeService: RecipeService){}
+
+    storeRecipes(){
+        const recipes = this.recipeService.getRecipes();
+        return this.http.put('https://angularcourseproject-6fcdd.firebaseio.com/recipes.json',
+                        recipes).subscribe(response=>{
+                            console.log(response);
+                        });
+    }
+    fetchRecipes(): Observable<Recipe[]> {
+        return this.http.get<Recipe[]>('https://angularcourseproject-6fcdd.firebaseio.com/recipes.json')
+        .pipe(map(recipes=>{
+            return recipes.map(recipe=>{
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients: []}
+            })
+        }),
+        tap(recipes=>{
+            this.recipeService.setRecipes(recipes);
+        })
+        );
+    }
+}
